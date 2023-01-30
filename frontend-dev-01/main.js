e9b9b5ec9e691fa2035fb11c3c84cb5ff79d929f
marialapc <marialopez.apc@gmail.com>
2023-01-30 22:10:06 +0100

    cart interactive step-1

--- frontend-dev-01/main.js
+++ frontend-dev-01/main.js
@@ -2,15 +2,30 @@
 const menuEmail = document.querySelector('.navbar-email');
 const desktopMenu = document.querySelector('.desktop-menu');
 const menuHamIcon = document.querySelector('.menu');
+const menuCartIcon = document.querySelector('.navbar-shopping-cart');
 const mobileMenu = document.querySelector('.mobile-menu');
+const aside = document.querySelector('.product-detail')
+
+
 
 menuEmail.addEventListener('click', toggleDesktopMenu);
 menuHamIcon.addEventListener('click', toggleMobileMenu);
+menuCartIcon.addEventListener('click', toggleCartAside);
+
 
 function toggleDesktopMenu() {
+    const isAsideClosed = aside.classList.contains('inactive');
+    if(!isAsideClosed){
+        aside.classList.add('inactive');
+    }
   desktopMenu.classList.toggle('inactive');
 }
 
 function toggleMobileMenu() {
-    mobileMenu.classList.toggle('inactive');
-  }
+ desktopMenu.classList.add('inactive')
+ mobileMenu.classList.toggle('inactive')
+}
+  function toggleCartAside () {
+  mobileMenu.classList.add('inactive')
+  .classList.toggle('inactive')
+  };
