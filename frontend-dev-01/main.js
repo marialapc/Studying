c63df13132c5f5979a915a060a5544847fbd74b5
marialapc <marialopez.apc@gmail.com>
2023-02-06 12:40:21 +0100

    paint the products

--- frontend-dev-01/main.js
+++ frontend-dev-01/main.js
@@ -5,7 +5,11 @@ const menuHamIcon = document.querySelector('.menu');
 const menuCartIcon = document.querySelector('.navbar-shopping-cart');
 const mobileMenu = document.querySelector('.mobile-menu');
 const aside = document.querySelector('.product-detail')
+const cardsContainer = document.querySelector('.cards-container')
 
+
+
+// Toggle the menus
 
 menuEmail.addEventListener('click', toggleDesktopMenu);
 menuHamIcon.addEventListener('click', toggleMobileMenu);
@@ -42,19 +46,65 @@ function toggleCarritoAside() {
 }
 
 
+// Paint the products
+
+const productList = [];
+productList.push({
+   name: 'Bike',
+   price: 120,
+   image: 'https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940',
+});
+productList.push({
+  name: 'pantalla',
+  price: 200,
+  image: 'https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940',
+});
+productList.push({
+  name: 'ordenador',
+  price: 600,
+  image: 'https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940',
+});
+productList.push({
+  name: 'Bike',
+  price: 120,
+  image: 'https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940',
+});
+productList.push({
+ name: 'pantalla',
+ price: 200,
+ image: 'https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940',
+});
+productList.push({
+ name: 'ordenador',
+ price: 600,
+ image: 'https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940',
+});
+
+function renderProducts(arr){
+  for (product of arr){
+const htmlCards = `<div class="product-card">
+<img src=${product.image} class="product-img">
+<div class="product-info">
+<div>
+<p>$${product.price}</p>
+<p>${product.name}</p>
+</div>
+<figure>
+<img src="./icons/bt_add_to_cart.svg" alt="">
+</figure>
+</div>
+<div>`
+cardsContainer.innerHTML += htmlCards
+}
+}
+renderProducts(productList)
+
+ 
 
 
 
 
 
-//  function toggleDesktopMenu(){
-//   aside.classList.add('inactive')
-//   mobileMenu.classList.toggle('inactive')
-//   desktopMenu.classList.toggle('inactive')
-//  };
 
-//  function toggleCartAside(){
-//   mobileMenu.classList.add('inactive')
-//   aside.classList.toggle('inactive')
-// desktopMenu.classList.add ('inactive')
-//  };
+
+
