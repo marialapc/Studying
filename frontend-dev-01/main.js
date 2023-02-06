95f652561ef79c1185ccb59c4a9c3c9348962300
marialapc <marialopez.apc@gmail.com>
2023-02-06 11:31:35 +0100

    toggle cart and email menu

--- frontend-dev-01/main.js
+++ frontend-dev-01/main.js
@@ -7,25 +7,54 @@ const mobileMenu = document.querySelector('.mobile-menu');
 const aside = document.querySelector('.product-detail')
 
 
-
 menuEmail.addEventListener('click', toggleDesktopMenu);
 menuHamIcon.addEventListener('click', toggleMobileMenu);
-menuCartIcon.addEventListener('click', toggleCartAside);
-
+menuCartIcon.addEventListener('click', toggleCarritoAside);
 
 function toggleDesktopMenu() {
-    const isAsideClosed = aside.classList.contains('inactive');
-    if(!isAsideClosed){
-        aside.classList.add('inactive');
-    }
+  const isAsideClosed = aside.classList.contains('inactive');
+
+  if (!isAsideClosed) {
+    aside.classList.add('inactive');
+  }
+  
   desktopMenu.classList.toggle('inactive');
 }
 
 function toggleMobileMenu() {
- desktopMenu.classList.add('inactive')
- mobileMenu.classList.toggle('inactive')
+  const isAsideClosed = aside.classList.contains('inactive');
+
+  if (!isAsideClosed) {
+    aside.classList.add('inactive'); 
+  }
+  
+  mobileMenu.classList.toggle('inactive');
 }
-  function toggleCartAside () {
-  mobileMenu.classList.add('inactive')
-  .classList.toggle('inactive')
-  };
+
+function toggleCarritoAside() {
+  const isMobileMenuClosed = mobileMenu.classList.contains('inactive');
+  
+  if (!isMobileMenuClosed) {
+    mobileMenu.classList.add('inactive'); 
+  }
+  
+  aside.classList.toggle('inactive');
+}
+
+
+
+
+
+
+
+//  function toggleDesktopMenu(){
+//   aside.classList.add('inactive')
+//   mobileMenu.classList.toggle('inactive')
+//   desktopMenu.classList.toggle('inactive')
+//  };
+
+//  function toggleCartAside(){
+//   mobileMenu.classList.add('inactive')
+//   aside.classList.toggle('inactive')
+// desktopMenu.classList.add ('inactive')
+//  };
